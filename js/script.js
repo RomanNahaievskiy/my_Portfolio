a414a83af284d0d138904171299623e39213949d
RomanNahaievskiy <nagaewsky.roma.s@gmail.com>
2023-12-16 01:49:51 +0200

    ++

--- js/script.js
+++ js/script.js
@@ -8,3 +8,27 @@ document.addEventListener("click", function (event) {
 });
 
 // decor
+const decorContEl = document.querySelector('.decor-digit-container');
+let div = document.createElement('div');
+div.className = 'decor-digit-columns';
+
+
+
+decorContEl.append(div);
+// клоную 8 разів
+for (let index = 0; index < 7; index++) {
+
+    decorContEl.append(document.querySelector('.decor-digit-columns').cloneNode(false));
+}
+// отримую елемент з колекції  та заповнюю його рандомними числами
+let objOfEl = document.querySelectorAll(".decor-digit-columns");
+
+objOfEl.forEach(element => {
+
+    for (let i = 0; i < 128; i++) {
+        // заповнюю ними віртуальний елемент
+        element.innerHTML += `<span>${Math.round(Math.random())}</span>`;
+    }
+});
+
+
